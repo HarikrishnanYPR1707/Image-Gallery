4fdc8177a3ad6375e13cb4c70f5397f448d69fa7
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-22 00:50:13 +0530

    Duplicating component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import "./App.css";
 import Card from "./components/ImageContainerCard/ImageContainerCard";
 
 function App() {
-  return <div className="bodyd">body</div>;
+  return (
+    <div className="bodyd">
+      {Array(1)
+        .fill(true)
+        .map((item, i) => {
+          return <Card />;
+        })}
+    </div>
+  );
 }
 
 export default App;
