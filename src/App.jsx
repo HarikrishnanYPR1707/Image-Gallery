99d6dc6eeec2430ce52bc97e5f78d4f2464a27a2
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-22 00:48:58 +0530

    Added the vite template and cleaned it up, added a component for card

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="bodyd">body</div>;
+}
+
+export default App;
---
+++ src/components/ImageContainerCard/ImageContainerCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.module.css";
+
+const ImageContainerCard = () => {
+  return <div className="body">ImageContainerCard</div>;
+};
+
+export default ImageContainerCard;
