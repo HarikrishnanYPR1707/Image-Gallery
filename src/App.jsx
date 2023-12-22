03405a93a4364bc6dee2f687946c4a75d6584938
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-23 01:11:41 +0530

    Added div manually for layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ function App() {
   return (
     <div className="wrapper">
       <div className="inner-wrapper">
-        {Array(5)
-          .fill(true)
-          .map((item, i) => {
-            return <Card key={i} />;
-          })}
+        <div className="card-container"></div>
+        <div className="card-container"></div>
+        <div className="card-container"></div>
+        <div className="card-container"></div>
+        <div className="card-container"></div>
+        <div className="card-container"></div>
+        <div className="card-container"></div>
+        <div className="card-container"></div>
       </div>
     </div>
   );
