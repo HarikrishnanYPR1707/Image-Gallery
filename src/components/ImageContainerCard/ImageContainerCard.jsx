db9716a37cc3248883b9460f73f3159faeef3d58
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-27 20:04:21 +0530

    Added image and division section

--- src/components/ImageContainerCard/ImageContainerCard.jsx
+++ src/components/ImageContainerCard/ImageContainerCard.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./style.module.css";
 
 const ImageContainerCard = ({ styles }) => {
-  return <div className={styles}>Card</div>;
+  return (
+    <div className={styles}>
+      {/* image  */}
+      <img src="" alt="" />
+      {/* black gradient division */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default ImageContainerCard;
