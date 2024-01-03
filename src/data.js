8f89178366791f3e123a56a71c23ed066a48b9d3
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-03 20:00:42 +0530

    Added path and title to data.js file

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const imageData = [
+  {
+    path: "./assets/images/img1.jpg",
+    title: "image 1",
+  },
+  {
+    path: "./assets/images/img2.jpg",
+    title: "image 2",
+  },
+  {
+    path: "./assets/images/img3.jpg",
+    title: "image 3",
+  },
+  {
+    path: "./assets/images/img4.jpg",
+    title: "image 4",
+  },
+  {
+    path: "./assets/images/img5.jpg",
+    title: "image 5",
+  },
+  {
+    path: "./assets/images/img6.jpg",
+    title: "image 6",
+  },
+  {
+    path: "./assets/images/img7.jpg",
+    title: "image 7",
+  },
+  {
+    path: "./assets/images/img8.jpg",
+    title: "image 8",
+  },
+];
