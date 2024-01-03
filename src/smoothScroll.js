6d624e4bf6e9020a83bedaad2f64ce8069eaa2c2
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-03 20:50:11 +0530

    Added smoothscroll.js

---
+++ src/smoothScroll.js
@@ -0,0 +1,12 @@
+import Lenis from "@studio-freight/lenis";
+
+export const smoothScroll = () => {
+  const lenis = new Lenis();
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+};
